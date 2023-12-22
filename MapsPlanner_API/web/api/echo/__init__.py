5473433fa7b277c4d84ef994bce7b6cd1d9d4152
Nimrod Wagner <nimrod7676@gmail.com>
2023-12-22 20:47:45 +0200

    Removed unused components

--- MapsPlanner_API/web/api/echo/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Echo API."""
-from MapsPlanner_API.web.api.echo.views import router
-
-__all__ = ["router"]
--- MapsPlanner_API/web/api/monitoring/__init__.py
+++
@@ -1,4 +0,0 @@
-"""API for checking project status."""
-from MapsPlanner_API.web.api.monitoring.views import router
-
-__all__ = ["router"]
