141fcf45f4fc17684412c0ae03666b34ca7fcc4d
Nimrod Wagner <nimrod7676@gmail.com>
2024-01-11 19:28:19 +0200

    Added user profile endpoint

--- MapsPlanner_API/web/api/users/schema.py
+++ MapsPlanner_API/web/api/users/schema.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 from pydantic import BaseModel
@@ -11,3 +12,10 @@ class User(BaseModel):
     profile_picture: Optional[str]
     is_active: bool
     is_administrator: bool
+
+
+class UserDetails(User):
+    register_date: datetime
+    fullname: str
+    total_trips: int
+    total_markers: int
--- MapsPlanner_API/web/api/users/views.py
+++ MapsPlanner_API/web/api/users/views.py
@@ -3,14 +3,17 @@ from typing import Annotated, Optional
 
 
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from starlette.requests import Request
 
 from MapsPlanner_API.db.dependencies import get_db_session
+from MapsPlanner_API.db.models.Marker import MarkerORM
 from MapsPlanner_API.db.models.Session import SessionORM
+from MapsPlanner_API.db.models.Trip import TripORM
 from MapsPlanner_API.db.models.User import UserORM
-from MapsPlanner_API.web.api.users.schema import User
+from MapsPlanner_API.web.api.users.schema import User, UserDetails
 
 logger = getLogger("app")
 
@@ -47,3 +50,42 @@ async def current_user(user: Annotated[UserORM, Depends(get_current_user)]) -> U
     Returns details of the currently connected user
     """
     return user.to_api()
+
+
+@router.get("/{user_id}")
+async def get_user_details(
+    user: Annotated[UserORM, Depends(get_current_user)],
+    db: Annotated[AsyncSession, Depends(get_db_session)],
+    user_id: int,
+) -> UserDetails:
+    not_found_exception = HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="No Such user."
+    )
+
+    if not (user_id == user.id or user.is_administrator):
+        raise not_found_exception
+
+    if user := await db.get(UserORM, user_id):
+        total_trips_query = (
+            select(func.count()).select_from(TripORM).where(TripORM.user_id == user_id)
+        )
+        total_trips_result = await db.execute(total_trips_query)
+
+        total_markers_query = (
+            select(func.count())
+            .select_from(MarkerORM)
+            .where(TripORM.user_id == user_id)
+            .join(TripORM, MarkerORM.trip_id == TripORM.id)
+        )
+
+        total_markers_result = await db.execute(total_markers_query)
+
+        return UserDetails(
+            **user.to_api().model_dump(),
+            register_date=user.register_date,
+            fullname=f"{user.first_name} {user.last_name}",
+            total_trips=total_trips_result.scalar(),
+            total_markers=total_markers_result.scalar(),
+        )
+    else:
+        raise not_found_exception
